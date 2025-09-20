1d4f2c8708e5abb0afb134fe50f3ec7e5fb89bfc
Zeazelu <zeazelu@gmail.com>
2025-09-20 12:49:34 +0200

    new link

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173", // dev
-      "https://essenza-frontend-57csy8vb7-zeazelus-projects.vercel.app", // produkcja (Vercel)
+      "https://essenza-frontend-ekch07hyj-zeazelus-projects.vercel.app", // produkcja (Vercel)
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
